dd5f42c96a20287e37694fb0dff85effd0a8ae59
Ashik Chapagain <ashik@pop-os.localdomain>
2022-04-12 17:43:41 +0545

    done ui

---
+++ packages/ui/Button.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+export const Button = () => {
+    return <button style={{
+        background: "red",
+        color: "white",
+        borderRadius: "5px",
+        padding: "10px"
+    }}>Button</button>;
+}
